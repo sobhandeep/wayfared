b4fbcb5891329413264643c3eb7985c8f74619b5
Sobhandeep Gope <130676586+sobhandeep@users.noreply.github.com>
2024-12-23 02:22:28 +0530

    Initial Commit

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { View } from "react-native";
+import LoginScreen from "@/screens/LoginScreen";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <LoginScreen/>
+    </View>
+  );
+}
