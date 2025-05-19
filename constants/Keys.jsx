b3ad5ef93e8ea336562782638b8e663f3b61dff9
sobhandeep <sobhandeep33@gmail.com>
2025-05-19 19:43:37 +0530

    build update

---
+++ constants/Keys.jsx
@@ -0,0 +1,8 @@
+export const GOOGLE_CLOUD_API = 'AIzaSyAd6XyvxkNQLHnP5a5nGolnuJF7PwP3aXQ'
+export const AUTH_DOMAIN = 'wayfared-55fb8.firebaseapp.com'
+export const PROJECT_ID = 'wayfared-55fb8'
+export const STORAGE_BUCKET = 'wayfared-55fb8.firebasestorage.app'
+export const MESSAGE_SENDING_ID = '352883144000'
+export const APP_ID = '1:352883144000:web:14be68b1b37c5b92ef76a5'
+export const GEMINI_API = 'AIzaSyAJQphAiaMZAJZrecKUqrDi__WTeMnh23I'
+export const SEARCH_ENGINE_ID = 'c495207680d624b92'
